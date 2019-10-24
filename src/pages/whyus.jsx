75460287c147b1e779a119902d24272adf951b13
thelovesmith <averydante9@gmail.com>
2019-10-24 17:48:24 -0400

    tomorrow is another day

--- src/pages/whyus.jsx
+++ src/pages/whyus.jsx
@@ -4,17 +4,18 @@ import { Header } from 'components';
 import { Layout } from 'layouts';
 import Model from '../components/why/Model';
 import Stats from '../components/why/Statistics';
-
+import Overview from '../components/why/Overview';
 
 const WhyUs = () => {
   return (
     <>
       <Layout>
         {/* <AboutRow1Wrap> */}
-          <Helmet title={'Why Capital Source 2000?'} />
-          <Header title="Why Us?"></Header>
-          <Model />
-          <Stats />
+        <Helmet title={'Why Capital Source 2000?'} />
+        <Header title="Why Us?"></Header>
+        <Model />
+        <Stats />
+        <Overview />
         {/* </AboutRow1Wrap> */}
       </Layout>
     </>
