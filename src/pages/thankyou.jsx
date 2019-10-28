857bc5a23d902cf3e7d734ae0c1cef3202216685
Kathleenmont <kathleen.m.montesano@gmail.com>
2019-10-28 14:30:47 -0400

    thank you page

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -3,19 +3,26 @@ import { Helmet } from 'react-helmet';
 import { Header } from 'components';
 import { Layout } from 'layouts';
 import Model from '../components/why/Model';
+import ThankYou from "../components/thankyou/Thankyou"
+import PropTypes from 'prop-types';
 
 import Overview from '../components/why/Overview';
 
-const ThankYou = () => {
+const ThankYouPage = () => {
   return (
     <>
       <Layout>
         <Helmet title={'Thank You'} />
         <Header title="Thank You"></Header>
-        <Model />
-        <Overview />
+        <ThankYou />
+       
       </Layout>
     </>
   );
 };
-export default ThankYou;
+
+ThankYouPage.propTypes = {
+  center: PropTypes.object,
+}; 
+
+export default ThankYouPage;
